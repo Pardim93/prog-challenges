97fb53801ec9900f410e3ea544d9f89f3d3662a1
Pardim93 <wellpardim10@gmail.com>
2019-06-04 00:55:16 -0300

    🤳🏿

--- hackerrank/warmup-challenges/sock_merchant.py
+++ hackerrank/warmup-challenges/sock_merchant.py
@@ -1,11 +1,15 @@
 
 def sockMerchant(n ,ar ):
     ar.sort()
+
+    [1 2 3  4 4 4 5]
     i = s = 0
     while i < n :
         c = j = 0
         while j < n: 
             if i == j:
+                break
+            if j > i and ar[i] != ar[j]:
                 break  
             if ar[i] == ar[j]:
                 c += 1
