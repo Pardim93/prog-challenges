5aa278f3f5363db17d6ba9d1198768ad616f0933
Pardim93 <wellpardim10@gmail.com>
2019-06-05 09:42:33 -0300

    f

--- hackerrank/warmup-challenges/sock_merchant.py
+++ hackerrank/warmup-challenges/sock_merchant.py
@@ -1,20 +1,10 @@
 
+from itertools import groupby
+
 def sockMerchant(n ,ar):
     ar.sort()
-   
-    #  it O(n), i promisse
-    i = s = 0
-    while i < n :
-        c = j = 0
-        while j < n: 
-            if i == j:
-                break
-            if j > i and ar[i] != ar[j]:
-                break  
-            if ar[i] == ar[j]:
-                c += 1
-                i = j 
-            j += 1
-        s += c // 2   
-        i += 1          
-    return s
+    total = 0
+    for key, group in groupby(ar, lambda x: int(x)):
+        total+= len(list(group)) // 2
+
+    return total
