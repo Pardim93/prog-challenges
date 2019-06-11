ca44187435bda22ec5222bf5672ef5162e1e8c6b
Pardim93 <wellpardim10@gmail.com>
2019-06-11 11:16:35 -0300

    h

---
+++ misc/age_seconds.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+def main():
+
+    year  = int(input("Enter your birth year:"))
+    month = int(input("Enter your month:"))
+    day   = int(input("Enter your day:"))
+
+    age = datetime.now() - datetime(year, month, day)
+    print(year.total_seconds())
+
+if __name__ == "__main__":
+    main()
