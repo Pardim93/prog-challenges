d75561162d706e4efa5c5097d43b001a4b2873e9
Pardim93 <wellpardim10@gmail.com>
2019-06-12 06:01:16 -0300

    high iq fizzbuzz

---
+++ misc/fizzbuzz.py
@@ -0,0 +1,10 @@
+def main():
+    fb = 'FizzBuzz'
+
+    for i in range(1, 1000):
+        tmp = str(i)
+        sub_fb = (fb[0+((i % 3 != 0) * 4):4+(i % 5 == 0) * 4])
+        print(sub_fb) if len(sub_fb) > 0 else print(i)
+
+if __name__ == "__main__":
+    main()
