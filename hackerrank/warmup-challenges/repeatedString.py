b5e237974956c7b23694b06a934c8209645e5fd9
Wellington PF <wellpardim10@gmail.com>
2019-09-13 19:18:31 -0300

    Update repeatedString.py

--- hackerrank/warmup-challenges/repeatedString.py
+++ hackerrank/warmup-challenges/repeatedString.py
@@ -1,6 +1,2 @@
 def repeatedString(s, n):
-    o = 0
-    for x in s:
-        if 'a' == x:
-            o += 1
-    return (n//len(s)) * o  +( s[:n%len(s):].count('a'))
+    return (n//len(s)) * s.count('a')  +( s[:n%len(s):].count('a'))
